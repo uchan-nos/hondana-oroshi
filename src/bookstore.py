2907d654b5c4a90d83145e30bb960c9a81236b0f
uchan-nos <uchan0@gmail.com>
2017-11-28 23:18:20 +0900

    add basic methods to bookstore

--- src/bookstore.py
+++ src/bookstore.py
@@ -6,3 +6,12 @@ import oroshi
 class Bookstore:
     def find_records_by_isbn(self, isbn: str) -> Iterable[oroshi.BookRecord]:
         raise NotImplementedError()
+
+    def get_record(self, record_id: int) -> oroshi.BookRecord:
+        raise NotImplementedError()
+
+    def add_record(self, record: oroshi.BookRecord):
+        raise NotImplementedError()
+
+    def update_record(self, record: oroshi.BookRecord):
+        raise NotImplementedError()
